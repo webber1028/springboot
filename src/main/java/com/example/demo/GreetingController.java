a7cbaa239eb5ff3dbb06de1648d3d00681030d76
webber1028 <webber1028@gmail.com>
2017-12-21 10:19:51 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	@RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello World";
+    }
+}
